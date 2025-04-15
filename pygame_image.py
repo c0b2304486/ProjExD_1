9749b04ba94aca90d08357974ce0d5670aafe4d4
kanno <c0b2304486@edu.teu.ac.jp>
2025-04-15 15:53:13 +0900

    練習10　うごいた

--- pygame_image.py
+++ pygame_image.py
@@ -15,15 +15,31 @@ def main():
     tmr = 0
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img,True,False)
+
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300,200
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
+            
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2,[1600+x,0])
         screen.blit(bg_img, [3200+x, 0])
-        screen.blit(kk_img,[300,200])
+        screen.blit(kk_img,kk_rct)
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0,+1))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1,0))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((+1,0))
+        
         pg.display.update()
+        
         x -= 1
         if x == -3199:
             x = 0
